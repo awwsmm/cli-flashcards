b3ea92b53a1ab810013448dff6c668b4cf6f036b
Andrew William Watson <aww@awwsmm.com>
2022-08-11 18:02:55 -0400

    list categories, return full question data

--- src/main/scala/FlashcardServiceImpl.scala
+++ src/main/scala/FlashcardServiceImpl.scala
@@ -3,19 +3,42 @@ package org.clif
 import akka.NotUsed
 import akka.grpc.GrpcServiceException
 import akka.stream.scaladsl.Source
+import com.google.protobuf.empty.Empty
 import io.grpc.Status
 import upickle.default.read
 
+import java.io.File
+import java.net.{JarURLConnection, URL, URLDecoder}
+import java.nio.file.{FileSystems, Files}
+import java.util.jar.JarFile
+import scala.jdk.CollectionConverters.*
 import scala.util.{Failure, Success, Try}
 
 class FlashcardServiceImpl extends FlashcardService:
+
+	// do some horrible classpath hacking to get list of files in /resources directory
+	override def categories(in: Empty): Source[Category, NotUsed] =
+
+		Try(getClass.getClassLoader.getResource("example.json")) match
+
+			case Failure(exception) =>
+				val message = s"Unable to list categories."
+				val status = Status.INTERNAL.withDescription(message)
+				throw new GrpcServiceException(status)
+
+			case Success(value) =>
+
+				val jarPath = value.getPath.substring(5, value.getPath.indexOf('!'))
+				val jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"))
+				val categories = jar.entries().asScala.filter(_.getName.endsWith(".json")).map(each => Category(each.getName.replace(".json", "")))
+				Source.fromIterator(() => categories)
 
 	override def flashcards(in: Category): Source[Flashcard, NotUsed] =
 
 		Try(scala.io.Source.fromResource(f"${in.name}.json")) match
 
 			case Failure(exception) =>
-				val message = s"Cannot find category file ${in.name}.json"
+				val message = s"Cannot find category file ${in.name}.json. See /Categories"
 				val status = Status.NOT_FOUND.withDescription(message)
 				throw new GrpcServiceException(status)
 
@@ -24,11 +47,11 @@ class FlashcardServiceImpl extends FlashcardService:
 
 				val json = fileSource.getLines().mkString("\n")
 
-				val questions = read[Array[MFlashcard[?]]](json).toSeq
+				val questions = read[Array[org.clif.model.Flashcard[?]]](json).toSeq
 
 				Source(questions).map {
 					_ match
-						case MMultipleChoice(prompt, choices) =>
+						case org.clif.model.MultipleChoice(prompt, choices) =>
 							Flashcard(in.name, prompt)
 								.withMultipleChoice(
 									MultipleChoice(
@@ -39,7 +62,7 @@ class FlashcardServiceImpl extends FlashcardService:
 									)
 								)
 
-						case MFillInTheBlank(prompt, matcher) =>
+						case org.clif.model.FillInTheBlank(prompt, matcher) =>
 							Flashcard(in.name, prompt)
 								.withFillInTheBlank(
 									FillInTheBlank(matcher.pattern.toString)
--- src/main/scala/model/Flashcard.scala
+++ src/main/scala/model/Flashcard.scala
@@ -1,25 +1,26 @@
 package org.clif
+package model
 
 import upickle.default.{macroRW, readwriter, ReadWriter as RW}
 
 import scala.util.matching.Regex
 
-sealed trait MFlashcard[T]:
+sealed trait Flashcard[T]:
 	def prompt: String
 	def validate(t: T): Boolean
 
-object MFlashcard:
-	given rw: RW[MFlashcard[?]] = RW.merge(MMultipleChoice.rw, MFillInTheBlank.rw)
+object Flashcard:
+	given rw: RW[Flashcard[?]] = RW.merge(MultipleChoice.rw, FillInTheBlank.rw)
 
-case class MMultipleChoice(prompt: String, choices: Map[String, Boolean]) extends MFlashcard[Seq[String]]:
+case class MultipleChoice(prompt: String, choices: Map[String, Boolean]) extends Flashcard[Seq[String]]:
 	override def validate(responses: Seq[String]): Boolean = choices.forall { case (choice, correct) => correct == responses.contains(choice) }
 
-object MMultipleChoice:
-	given rw: RW[MMultipleChoice] = macroRW
+object MultipleChoice:
+	given rw: RW[MultipleChoice] = macroRW
 
-case class MFillInTheBlank(prompt: String, matcher: Regex) extends MFlashcard[String]:
+case class FillInTheBlank(prompt: String, matcher: Regex) extends Flashcard[String]:
 	override def validate(response: String): Boolean = matcher.matches(response)
 
-object MFillInTheBlank:
+object FillInTheBlank:
 	given rwRegex: RW[Regex] = readwriter[String].bimap[Regex](_.pattern.toString, new Regex(_))
-	given rw: RW[MFillInTheBlank] = macroRW
+	given rw: RW[FillInTheBlank] = macroRW
